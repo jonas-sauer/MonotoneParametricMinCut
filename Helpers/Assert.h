bbcf44bcca76224e18bcc5980a98b341dfd27e5a
jsauer <jsauer1@uni-bonn.de>
2024-08-22 17:44:23 +0200

    Remove unused method in Assert.h

--- Helpers/Assert.h
+++ Helpers/Assert.h
@@ -10,12 +10,3 @@ inline void ensure(const bool assumption) noexcept {
 
 #define Assert(assumption, msg) assert((assumption) || (std::cout << "\n\033[31mASSERTION FAILED: " << msg << "\033[0m\nFile: " << __FILE__ << "\nLine: " << __LINE__ << "\n" << std::flush && false))
 #define Ensure(assumption, msg) ensure((assumption) || (std::cout << "\n\033[31mERROR: " << msg << "\033[0m\nFile: " << __FILE__ << "\nLine: " << __LINE__ << "\n" << std::flush && false))
-
-void checkAsserts() {
-#ifdef NDEBUG
-    std::cout << "Running in release-mode!" << std::endl;
-    assert(false);
-#else
-    std::cout << "Running with active assertions!" << std::endl;
-#endif
-}
